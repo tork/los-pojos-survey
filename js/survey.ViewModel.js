35abf0c0bcf1dd6c2f86b166d5225df9bd14aa7f
Marianne Grov <mariannegrov@gmail.com>
2013-11-05 18:20:52 +0100

    added some test data

--- js/survey.ViewModel.js
+++ js/survey.ViewModel.js
@@ -1,22 +1,29 @@
 (function(root){
-// Kan nok rename denne etter hvert som vi vet hvilke viewModels vi trenger - iallfall hvis vi trenger flere!
-    var viewModel = function() {
-        var self = this;
-        //må pushe objecter med minimum en prop kalt name i programs og programStages
-        //Kan være lurt å ha en model som ser slik ut kanskje: {name: "Navnet", id: "idsomething"}s
-        self.programs = ko.observableArray(); //populate with get
-        self.selectedProgram = ko.observable();
-        self.programStages = ko.observableArray(); //populate with get
-        self.selectedProgramStage = ko.observable();
-        self.dataElements = ko.observableArray();
-    }
+//	Kan nok rename denne etter hvert som vi vet hvilke viewModels vi trenger - iallfall hvis vi trenger flere!
+	var viewModel = function() {
+		var self = this;
+		//må pushe objecter med minimum en prop kalt name i programs og programStages
+		//Kan være lurt å ha en model som ser slik ut kanskje: {name: "Navnet", id: "idsomething"}s
+		self.programs = ko.observableArray();
+		self.selectedProgram = ko.observable();
+		self.programStages = ko.observableArray();
+		self.selectedProgramStage = ko.observable();
+		self.dataElements = ko.observableArray();
 
-    /*
-    * Litt forklaring: selectedProgram vil inneholde det man har valgt,
-    * eller undefined hvis det står "Select program" i selecten.
-    * Det er en observable, så den hentes ut ved å kalle viewModel.selectedProgram()
-    * Foreslår at dette brukes til å gjøre get til api.
-    * */
 
-    root.viewModel = new viewModel();
+		/* testdata */
+//		self.programStages = ko.observableArray([{"name" : "stage1"}, {"name" : "stage2"}, {"name" : "stage3"}]); //populate with get
+//		self.programs = ko.observableArray([{"name" : "prog1"}, {"name" : "prog2"}, {"name" : "prog3"}]); //populate with get
+//		self.dataElements.push(ko.observable({"value": "22", "name":"Age","created":"2012-11-14T13:41:39.639+0000","lastUpdated":"2013-03-15T16:32:26.280+0000","shortName":"Age","description":"Age of patient.","formName":"Age (Years)","active":true,"domainType":"patient","type":"int","numberType":"int","aggregationOperator":"sum","categoryCombo":{"name":"default","created":"2011-12-24T11:24:25.203+0000","lastUpdated":"2011-12-24T11:24:25.203+0000","href":"http://apps.dhis2.org/demo/api/categoryCombos/p0KPaWEg3cf","id":"p0KPaWEg3cf"},"url":"","zeroIsSignificant":false,"optionSet":null,"legendSet":null,"access":{"manage":true,"externalize":false,"write":true,"read":true,"update":true,"delete":true},"href":"http://apps.dhis2.org/demo/api/dataElements/qrur9Dvnyt5","id":"qrur9Dvnyt5"}));
+//		self.dataElements.push(ko.observable({"value": "Male","name":"Gender","created":"2012-11-14T12:11:55.105+0000","lastUpdated":"2013-03-15T16:32:26.287+0000","shortName":"Gender","description":"Gender of patient.","formName":"Gender","active":true,"domainType":"patient","type":"string","textType":"text","aggregationOperator":"sum","categoryCombo":{"name":"default","created":"2011-12-24T11:24:25.203+0000","lastUpdated":"2011-12-24T11:24:25.203+0000","href":"http://apps.dhis2.org/demo/api/categoryCombos/p0KPaWEg3cf","id":"p0KPaWEg3cf"},"url":"","zeroIsSignificant":false,"optionSet":{"name":"Gender","created":"2012-11-13T08:08:51.450+0000","lastUpdated":"2012-11-13T08:08:51.450+0000","href":"http://apps.dhis2.org/demo/api/optionSets/fEDqAkq2X4o","id":"fEDqAkq2X4o"},"legendSet":null,"access":{"manage":true,"externalize":false,"write":true,"read":true,"update":true,"delete":true},"href":"http://apps.dhis2.org/demo/api/dataElements/oZg33kd9taw","id":"oZg33kd9taw"}));
+	}
+
+	/*
+	 * Litt forklaring: selectedProgram vil inneholde det man har valgt,
+	 * eller undefined hvis det står "Select program" i selecten.
+	 * Det er en observable, så den hentes ut ved å kalle viewModel.selectedProgram()
+	 * Foreslår at dette brukes til å gjøre get til api.
+	 * */
+
+	root.viewModel = new viewModel();
 })(survey);

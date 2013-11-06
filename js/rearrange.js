2d82aacd825fb5a2b79095d1e4f540e957f1b64a
tork <torkil.aamodt@gmail.com>
2013-11-06 09:54:07 +0100

    Morgencommit: rearrange.js now cleans up all elements which are unlockable (i.e. not elements with circular dependencies). Dependency descriptors are also appended.

--- js/rearrange.js
+++ js/rearrange.js
@@ -3,32 +3,65 @@ Takes an unordered array of data elements, and
 returns them ordered based on their dependencies.
 
 Usage: var arranged = rearrange(unordered_array);
+
+Array 'dependencies' is added to all elements;
+elements with no dependencies have an empty array.
+
+Elements with circular dependencies (i.e impossible
+to unlock) are excluded from the rearrangement.
 */
 
 var create_workspace = create_workspace_array;
 var get_element = get_element_array;
 var extract_arrangement = extract_arrangement_array;
 
-var test_set = [
-{
-	element_id: 10,
-	unreadable_id: 20
-},{
-	element_id: 11,
-	unreadable_id: 21
-},{
-	element_id: 12,
-	unreadable_id: 22
-},{
-	element_id: 13,
-	unreadable_id: 23
-}
-];
+(function testbed() {
+	var test_set = [
+	{
+		element_id: 10,
+		unreadable_id: 20
+	},{
+		element_id: 11,
+		unreadable_id: 21
+	},{
+		element_id: 12,
+		unreadable_id: 22
+	},{
+		element_id: 13,
+		unreadable_id: 23
+	},{
+		// Circular dependencies (excluded)
+		element_id: -10,
+		unreadable_id:-20
+	},{
+		// Isolated node
+		element_id: 19,
+		unreadable_id: 29
+	}
+	];
 
-var arr = rearrange(test_set);
-print("OLD\tNEW");
-for (var idx in arr) {
-	print(test_set[idx].element_id+'\t'+arr[idx].element_id);
+	debug(test_set);
+})();
+
+// Rearrange test_set and print debugging information
+function debug(test_set) {
+	var arr = rearrange(test_set);
+	
+	print("OLD\tNEW");
+	for (var idx in test_set) {
+		var elem = arr[idx];
+		print(test_set[idx].element_id+
+			(elem? '\t'+arr[idx].element_id:""));
+	}
+
+	print("\nKEPT ELEMENTS");
+	for (var idx in arr) {
+		var keys = Object.keys(arr[idx]);
+		keys.forEach(function(key) {
+			print(key+"\t"+arr[idx][key]);
+		});
+		print();
+	}
 }
 
 function rearrange(elements) {
@@ -43,7 +76,7 @@ function rearrange(elements) {
 }
 
 function fetch_dependencies(elem) {
-	//TODO: Actually execute a fetch
+	// TODO: Actually execute a fetch
 	
 	var deps = [];
 	switch (elem.element_id) {
@@ -60,6 +93,9 @@ function fetch_dependencies(elem) {
 		deps = [{
 			dep_id: 10,
 			triggers: [""]
+		},{
+			dep_id: -10,
+			triggers: [""]
 		}];
 		break;
 	case 12:
@@ -70,12 +106,21 @@ function fetch_dependencies(elem) {
 		break;
 	case 13:
 		break;
+	case -10:
+		deps = [{
+			dep_id: 11,
+			triggers: [""]
+		}];
+		break;
+	case 19:
+		break;
 	}
 	
 	return deps;
 }
 
 function register_dependencies(elem, deps, workspace) {
+	elem.dependencies = deps;
 	elem.dep_count = deps.length;
 	
 	if (elem.dep_count == 0) {
@@ -117,8 +162,8 @@ function extract_element(elem, arrangement) {
 		child = child.next;
 	}
 	
-	//TODO: This will cause unreachable nodes
-	// to never get cleaned. Should improve.
+	// TODO: This will cause unreachable nodes
+	// to never get cleaned. Should improve?
 	clean_element(elem);
 	
 	return arrangement;
@@ -127,11 +172,14 @@ function extract_element(elem, arrangement) {
 // Removes an elements temporary members
 // used by rearrange.js privately.
 function clean_element(elem) {
-	//TODO
+	delete elem.dependents;
+	delete elem.dep_count;
+	delete elem.prev;
+	delete elem.next;
 }
 
 
-// ARRAY AS WORKSPACE
+/** ARRAY AS WORKSPACE **/
 function create_workspace_array(elements) {
 	var workspace = {};
 	workspace.elements = elements;
@@ -157,13 +205,14 @@ function extract_arrangement_array(workspace) {
 	var arrangement = [];
 	
 	var root = workspace.free_queue;
-	if (!root) return arrangement;
-	do {
+	while (root) {
+		var tmp = root.next;
 		extract_element(root, arrangement);
-	} while (root = root.next);
+		root = tmp;
+	}
 	
 	return arrangement;
 }
 
 
-//TODO: HASH TABLE (?) AS WORKSPACE
+/** TODO: HASH TABLE (?) AS WORKSPACE **/

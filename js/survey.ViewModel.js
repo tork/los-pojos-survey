cccdea6484513fa3daf1456c747b7ac3b8ea2fff
Marianne Grov <mariannegrov@gmail.com>
2013-11-06 14:20:12 +0100

    implemented dependenciesHold

--- js/survey.ViewModel.js
+++ js/survey.ViewModel.js
@@ -18,20 +18,10 @@
 			self.value = ko.observable(false);
 		else
 			self.value = ko.observable("");	
-
-		self.dependenciesHolds = ko.computed(function() {
-			return true;
-
-//			$.each(self.dependencies, function( index, dep ) {
-//				var dataElement = root.viewModelgetDataElementByID(dep.id); //undefined
-//				if(!$.inArray(dep.values, dataElement.value())) return false;
-//				});
-//
-//			});
-//			return true;
-		});
-
 	}
+	
+
+
 
 //	Kan nok rename denne etter hvert som vi vet hvilke viewModels vi trenger - iallfall hvis vi trenger flere!
 	var viewModel = function() {
@@ -52,9 +42,16 @@
 		self.dataElements.push(ko.observable(new DataElement({"name":"Alive","created":"2012-11-14T12:11:55.105+0000","lastUpdated":"2013-03-15T16:32:26.287+0000","shortName":"Alive","description":"","formName":"Are you still alive?","active":true,"domainType":"patient","type":"checkbox","textType":"text","aggregationOperator":"sum","categoryCombo":{"name":"default","created":"2011-12-24T11:24:25.203+0000","lastUpdated":"2011-12-24T11:24:25.203+0000","href":"http://apps.dhis2.org/demo/api/categoryCombos/p0KPaWEg3cf","id":"p0KPaWEg3cf"},"url":"","zeroIsSignificant":false,"optionSet":{"name":"Gender","created":"2012-11-13T08:08:51.450+0000","lastUpdated":"2012-11-13T08:08:51.450+0000","href":"http://apps.dhis2.org/demo/api/optionSets/fEDqAkq2X4o","id":"fEDqAkq2X4o"},"legendSet":null,"access":{"manage":true,"externalize":false,"write":true,"read":true,"update":true,"delete":true},"href":"http://apps.dhis2.org/demo/api/dataElements/oZg33kd9taw","id":"checkBoxElementID"})));	
 
 		self.getDataElementByID = function(id) {
-			var result = $.grep(self.dataElements(), function(element){ return element.id == id; });
-			return result[0];
+			return undefined;
 		}
+		
+		self.dependenciesHold = function (dataelement) {
+			$.each(dataelement.dependencies, function( index, dep ) {
+				var dataElement = self.getDataElementByID(dep.id); //undefined
+				if(!dataElement == undefined && !$.inArray(dep.values, dataElement.value())) return false;
+			});
+			return true;
+		};
 	}
 
 	/*
@@ -64,6 +61,11 @@
 	 * Foreslår at dette brukes til å gjøre get til api.
 	 * */
 
+	
+	
 	root.viewModel = new viewModel();
 
+
+
+	
 })(survey);

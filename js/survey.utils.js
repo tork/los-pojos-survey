c8f39d1a6ab0842785b98c907ae102e2594d0b2d
kjohann <kristian.johannessen.1990@gmail.com>
2013-11-11 11:27:45 +0100

    Fixed url error in utils

--- js/survey.utils.js
+++ js/survey.utils.js
@@ -1,5 +1,5 @@
 (function(root){
     root.getBaseUrl = function () {
-        return window.location.origin == "file://" ? "localhost:8082" : "apps.dhis.org/demo";
+        return window.location.origin == "file://" ? "localhost:8082" : "apps.dhis2.org/demo";
     }
 })(survey.utils = survey.utils || {})

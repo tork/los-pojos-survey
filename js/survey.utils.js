0e6412bef5736032a04cbd50c581027639c431bd
kjohann <kristian.johannessen.1990@gmail.com>
2013-11-12 16:15:57 +0100

    Added util function for formatting date for input elements

--- js/survey.utils.js
+++ js/survey.utils.js
@@ -1,7 +1,7 @@
 (function(root){
     root.getBaseUrl = function () {
         return window.location.origin == "file://" ? "localhost:8082" : "apps.dhis2.org/demo";
-    }
+    };
 
     root.surveySettingsUrl = function(surveyId, loc) {
     	return
@@ -10,5 +10,15 @@
     		surveyId +
     		'.' +
     		loc;
-    }
+    };
+
+    root.getDateFormattedForInput = function (date) {
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        month = month < 10 ? "0" + month : month;
+        var day = date.getDate();
+        day = day < 10 ? "0" + day : day;
+
+        return year + "-" + month + "-" + day;
+    };
 })(survey.utils = survey.utils || {})

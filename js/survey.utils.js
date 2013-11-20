2aa1bf4a3315bdfed8b94af9a20a9477a1c19660
kjohann <kris-90@live.no>
2013-11-20 13:35:13 +0100

    Updated handling of date values to correspond with dhis

--- js/survey.utils.js
+++ js/survey.utils.js
@@ -29,15 +29,5 @@
     		loc;
     };
 
-    root.getDateFormattedForInput = function (date) {
-        var year = date.getFullYear();
-        var month = date.getMonth() + 1;
-        month = month < 10 ? "0" + month : month;
-        var day = date.getDate();
-        day = day < 10 ? "0" + day : day;
-
-        return year + "-" + month + "-" + day;
-    };
-
     root.debug = false;
 })(survey.utils = survey.utils || {})

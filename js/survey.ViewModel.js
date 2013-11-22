01af0d3654a870a64f23f7dc934a8d1772d5a436
Øyvind Tangen <oyvindrtangen@gmail.com>
2013-11-22 12:41:07 +0100

    Fixed a bug that caused an error when switching to a new program from an empty program stage

--- js/survey.ViewModel.js
+++ js/survey.ViewModel.js
@@ -24,9 +24,11 @@
 			self.selectedProgramStagesOptionSets().length = 0;
 			self.downloadedDataElements().length = 0;
 			self.dataElements().length = 0;
-			if (self.selectedProgramStage().programStageDataElements) {
-				survey.data.getAllDataElementsForSelectedProgramStage();
-				survey.data.getOrgUnits(self.selectedProgram().id);
+			if (self.selectedProgramStage()) {
+				if (self.selectedProgramStage().programStageDataElements) {
+					survey.data.getAllDataElementsForSelectedProgramStage();
+					survey.data.getOrgUnits(self.selectedProgram().id);
+				}
 			}
 		});
 		self.selectedProgramStagesOptionSets = ko.observableArray();
